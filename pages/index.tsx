4c923f4c4871e3fda960cbdc8f050d2dc67ba1dd
Yusuf Arslan <yusufarsln98@outlook.com>
2023-09-20 18:11:17 +0300

    initialize todo-list

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Todo List</title>
+        <meta
+          name="description"
+          content="
+          A simple todo list app built with Next.js, SASS and TypeScript.
+        "
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} `}>
+        <>hello</>
+      </main>
+    </>
+  );
+}
